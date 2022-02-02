a7c87b937b0b312dbe680013eb7bfa146306b4a7
Marjoree <65003463+mfargas@users.noreply.github.com>
2022-02-01 18:35:16 -0800

    test

--- js/fun.js
+++ js/fun.js
@@ -105,7 +105,7 @@ const fetchExercises = async (objRequest) => {
             allExercises = allExercises.map(exercise => ({
                 description: exercise.description,
                 duration: exercise.duration,
-                date: exercise.toDateString()
+                date: exercise.date.toDateString()
             }))
         }
 
